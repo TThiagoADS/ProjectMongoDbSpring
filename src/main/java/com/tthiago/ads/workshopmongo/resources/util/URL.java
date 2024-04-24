d841da3fff1ca0df3c89f3119bcd875d6c88f745
TTHiagoADS <thiagoubatuba123@gmail.com>
2024-04-24 10:09:48 -0300

    finish project

--- src/main/java/com/tthiago/ads/workshopmongo/resources/util/URL.java
+++ src/main/java/com/tthiago/ads/workshopmongo/resources/util/URL.java
@@ -2,6 +2,12 @@ package com.tthiago.ads.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.springframework.boot.autoconfigure.task.TaskExecutionProperties.Simple;
 
 public class URL {
 	
@@ -15,4 +21,13 @@ public class URL {
 		}
 	}
 
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
+		}
+	}
 }
